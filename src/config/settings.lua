f4f12ff4d767dfe255a7fb8cc2ef10152d3dbd13
Xannaeh <44808157+Xannaeh@users.noreply.github.com>
2025-07-09 11:14:55 +0200

    fix: update powerDevice UUID to actual battery buffer address

--- src/config/settings.lua
+++ src/config/settings.lua
@@ -8,12 +8,13 @@ local Settings = {}
 Settings.updateInterval = 2
 
 -- Power storage: GT Battery Buffer UUID
-Settings.powerDevice = "afb812d4-a11f-498b-8f75-4d3cdb62e7f4"
+Settings.powerDevice = "e3f5877c-5dce-4bb9-98d4-922084dedf59"
 
 -- Fluid Tanks: one GT machine UUID
 Settings.fluidTanks = {
     main = "a45c6d6e-930c-41ef-a98b-97869ad1d667"
 }
+
 
 -- Screen settings
 Settings.screenResolution = { width = 80, height = 25 }
